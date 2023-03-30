93e914156e790bb6981e705951d079fe4a656853
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2023-03-30 21:34:33 +0300

    Switch to poetry setup

--- filedialogs/__init__.py
+++ filedialogs/__init__.py
@@ -1,1 +1,7 @@
-from .filedialogs import *
+from .filedialogs import open_file_dialog, open_folder_dialog, save_file_dialog
+
+__all__ = [
+    "open_file_dialog",
+    "open_folder_dialog",
+    "save_file_dialog",
+]
--- filedialogs/filedialogs.py
+++ filedialogs/filedialogs.py
@@ -1,12 +1,12 @@
 """File dialog functions."""
 
 import os
-import pywintypes
-from typing import Union, List, Optional, Tuple
-from win32gui import GetDesktopWindow, GetOpenFileNameW, GetSaveFileNameW
-from win32com.shell import shell, shellcon
-from win32con import OFN_EXPLORER, OFN_ALLOWMULTISELECT
+from typing import List, Optional, Tuple, Union
 
+import pywintypes
+from win32com.shell import shell, shellcon  # type: ignore
+from win32con import OFN_ALLOWMULTISELECT, OFN_EXPLORER
+from win32gui import GetDesktopWindow, GetOpenFileNameW, GetSaveFileNameW
 
 __all__ = [
     "open_file_dialog",
@@ -17,7 +17,7 @@ __all__ = [
 
 def open_file_dialog(
     title: str = None,
-    directory: str = os.getcwd(),
+    directory: Optional[str] = None,
     default_name: str = "",
     default_ext: str = "",
     ext: List[Tuple[str, str]] = None,
@@ -33,12 +33,15 @@ def open_file_dialog(
                 e.g. [(JPEG Image, jpg), (PNG Image, png)].
     :param multiselect: Allow multiple files to be selected.
     :return: Path to a file to open if multiselect=False.
-             List of the paths to files which should be opened if multiselect=True.
+             List of the paths to files, which should be opened if multiselect=True.
              None if file open dialog canceled.
     :raises IOError: File open dialog failed.
     """
 
     # https://programtalk.com/python-examples/win32gui.GetOpenFileNameW/
+
+    if directory is None:
+        directory = os.getcwd()
 
     flags = OFN_EXPLORER
     if multiselect:
@@ -55,7 +58,7 @@ def open_file_dialog(
             File=default_name,
             Flags=flags,
             Title=title,
-            MaxFile=2 ** 16,
+            MaxFile=2**16,
             Filter=ext,
             DefExt=default_ext,
         )
@@ -71,16 +74,16 @@ def open_file_dialog(
 
         return paths
 
-    except pywintypes.error as e:  # noqa
-        if e.winerror == 0:
+    except pywintypes.error as error:  # noqa
+        if error.winerror == 0:
             return
         else:
-            raise IOError()
+            raise IOError() from error
 
 
 def save_file_dialog(
     title: str = None,
-    directory: str = os.getcwd(),
+    directory: Optional[str] = None,
     default_name: str = "",
     default_ext: str = "",
     ext: List[Tuple[str, str]] = None,
@@ -93,11 +96,14 @@ def save_file_dialog(
     :param default_ext: Default file extension. Only letters, no dot.
     :param ext: List of available extension description + name tuples,
                 e.g. [(JPEG Image, jpg), (PNG Image, png)].
-    :return: Path file should be save to. None if file save dialog canceled.
+    :return: Path file should be saved to. None if file save dialog canceled.
     :raises IOError: File save dialog failed.
     """
 
     # https://programtalk.com/python-examples/win32gui.GetSaveFileNameW/
+
+    if directory is None:
+        directory = os.getcwd()
 
     if ext is None:
         ext = "All Files\0*.*\0"
@@ -109,18 +115,18 @@ def save_file_dialog(
             InitialDir=directory,
             File=default_name,
             Title=title,
-            MaxFile=2 ** 16,
+            MaxFile=2**16,
             Filter=ext,
             DefExt=default_ext,
         )
 
         return file_path
 
-    except pywintypes.error as e:
-        if e.winerror == 0:
+    except pywintypes.error as error:
+        if error.winerror == 0:
             return
         else:
-            raise IOError()
+            raise IOError() from error
 
 
 def open_folder_dialog(title: str = "", encoding: str = "ISO8859-1") -> Optional[str]:
@@ -134,9 +140,7 @@ def open_folder_dialog(title: str = "", encoding: str = "ISO8859-1") -> Optional
     # http://timgolden.me.uk/python/win32_how_do_i/browse-for-a-folder.html
 
     desktop_pidl = shell.SHGetFolderLocation(0, shellcon.CSIDL_DESKTOP, 0, 0)
-    pidl, display_name, image_list = shell.SHBrowseForFolder(
-        GetDesktopWindow(), desktop_pidl, title, 0, None, None
-    )
+    pidl, display_name, image_list = shell.SHBrowseForFolder(GetDesktopWindow(), desktop_pidl, title, 0, None, None)
 
     if (pidl, display_name, image_list) != (None, None, None):
         return shell.SHGetPathFromIDList(pidl).decode(encoding)
